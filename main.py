56dfe37c35b833e92a3cd4e29852e8799cbadd50
tocchimittsu <arimagedon21@gmail.com>
2021-06-18 18:46:08 +0900

    修正

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import tkinter as tk
 
 
 class Application(tk.Frame):
-    # entry用変数
+    # entry Widget用変数
     entry = None
 
-    # 入力検証に失敗した場合に呼ばれる関数
+    # 入力制限に失敗した場合に呼ばれる関数
     def invalidText(self):
         print('だめ!大文字入力!絶対！！')
 
-    # 入力検証するための関数
-    # 入力された文字に対して入力検証を行う。
+    # 入力制限すべきか判定するための関数
+    # 入力された文字に対して入力制限を行う。
     # 入力された文字が大文字ならば、"入力不正"とみなし、entry Widget内へ入力された文字を追加しない。
     # 入力された文字が大文字ならば、ブザーを鳴らす。
     def onValidate(self, d, i, P, s, S, v, V, W):
         ################################################################
-        # 入力検証のために、利用できるデータ内容をtext Widgetへ書き込む。
+        # 入力制限のために、利用できるデータ内容をtext Widgetへ書き込む。
         # d : テキストボックスを操作したアクション内容(1=insert(文字を追加した), 0=delete(文字を削除した), -1(その他))
         # i : 現在テキストボックスに格納される、文字列の長さ + 1(文字列indexの長さ)
         # P : 現在テキストボックスに格納される、文字列。
@@ -68,7 +68,7 @@ class Application(tk.Frame):
         # frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
         frame = tk.Frame(self.master)
 
-        # Windowを親要素とした場合に、frame Widgetをどのように配置するのか?
+        # Windowを親要素とした場合に、frame Widget(Frame)をどのように配置するのか?
         # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
         frame.pack()
 
@@ -76,7 +76,7 @@ class Application(tk.Frame):
 
         ############################################## text Widget START ##############################################
 
-        # frame Widget(Frame)を親要素として、入力検証のために、利用できるデータ内容を書き込むtext Widgetを作成する。
+        # frame Widget(Frame)を親要素として、入力制限のために、利用できるデータ内容を書き込むtext Widgetを作成する。
         # height : 高さの設定
         # width : 幅の設定
         # Textについて : https://kuroro.blog/python/bK6fWsP9LMqmER1CBz9E/
@@ -90,9 +90,9 @@ class Application(tk.Frame):
 
         ############################################## entry Widget START ##############################################
 
-        # 入力検証するための関数登録と、入力検証するために必要なデータ宣言を行う。
-        # register : 入力検証用関数の登録を行う。
-        # self.onValidate : 入力検証用関数
+        # 入力制限を判定するための関数登録と、入力制限するために必要なデータの宣言を行う。
+        # register : 入力制限を行うための関数登録を行う。
+        # self.onValidate : 入力制限すべきか判定するための関数
         #
         # %d = テキストボックスを操作したアクション内容(1=insert(文字を追加した), 0=delete(文字を削除した), -1(その他))
         # %i = 現在テキストボックスに格納される、文字列の長さ + 1(文字列indexの長さ)
@@ -107,15 +107,15 @@ class Application(tk.Frame):
 
         # frame Widget(Frame)を親要素として、entry Widgetを作成する。
         # width : 幅の設定
-        # validate : 入力検証するオプションの値を設定。
-        # validatecommand : 入力検証用関数の設定。
-        # invalidcommand : 入力検証により、入力不正が発生した場合に呼ばれる関数の設定。
+        # validate : 入力制限するオプションの値を設定。
+        # validatecommand : 入力制限用関数の設定。
+        # invalidcommand : 入力制限により、入力不正が発生した場合に呼ばれる関数の設定。
         ##################################### validateのオプションの種類 ###############################################
-        # none : 入力検証しない。
-        # focus : entry Widgetへカーソルがあたった、カーソルが外れた場合に入力検証する。
-        # focusin : entry Widgetへカーソルがあたった場合に入力検証する。
-        # focusout : entry Widgetへカーソルが外れた場合に入力検証する。
-        # key : entry Widgetへ文字の追加、削除が行われた場合に入力検証する。
+        # none : 入力制限しない。
+        # focus : entry Widgetへカーソルがあたった、カーソルが外れた場合に入力制限する。
+        # focusin : entry Widgetへカーソルがあたった場合に入力制限する。
+        # focusout : entry Widgetへカーソルが外れた場合に入力制限する。
+        # key : entry Widgetへ文字の追加、削除が行われた場合に入力制限する。
         # all : focus, focusin, focusout, key全て。
         # Entryについて : https://kuroro.blog/python/PUZp77YFxrXvMCjpZbUg/
         ############################################################################################################
